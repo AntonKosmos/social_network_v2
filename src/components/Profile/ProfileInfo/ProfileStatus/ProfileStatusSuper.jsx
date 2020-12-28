a9a1e5f464e0e54611d46a67477774bdf90a95a8
Anton <anton@brightdot.tech>
2020-12-28 15:26:32 +0300

    fix

---
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusSuper.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+class ProfileStatusSuper extends React.Component {
+    state = {
+        editMode: false
+    }
+
+    onActiveMode  = () => {
+        this.setState({
+            editMode: true
+        });
+    }
+
+    onDeactivation = () => {
+        this.setState({
+            editMode: false
+        })
+    }
+
+    render() {
+        return <div>
+
+            {!this.state.editMode &&
+            <div>
+                <span onDoubleClick={() => {this.onActiveMode()}}>{this.props.status}</span>
+            </div>}
+            {this.state.editMode &&
+            <div>
+                    <input onBlur={()=> {this.onDeactivation()}} autoFocus={true} value={this.props.status}/>
+            </div>}
+
+        </div>
+    }
+}
+
+export default ProfileStatusSuper;
